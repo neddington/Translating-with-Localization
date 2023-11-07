c3d3ed033d17eaabfe10a0f9b18a7a4f74e7b9d4
neddington <neddington@cascadia.edu>
2023-11-06 17:35:38 -0800

    Initial Commit

---
+++ Translating with Localization/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Translating with Localization
+//
+//  Created by Eddington, Nick on 11/6/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Translating with Localization/Translating_with_LocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Translating_with_LocalizationApp.swift
+//  Translating with Localization
+//
+//  Created by Eddington, Nick on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Translating_with_LocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
