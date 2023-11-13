970d1eb57d6220bedb1a596ce5e2739dc7b36976
neddington <neddington@cascadia.edu>
2023-11-13 15:46:24 -0800

    Updated

--- Translating with Localization/ContentView.swift
+++ Translating with Localization/ContentView.swift
@@ -6,19 +6,41 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State var changeColor = false
+    @State var name = ""
+    @State var buttonNumber = 1
+    let placeholder: LocalizedStringKey = "placeholder-label"
+    let value = 3.1415
+    let date: Date
+    let dateFormatter: DateFormatter
+    init() {
+        date = Date()
+        dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .medium
+    }
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+            Text("greeting-label \(name)")
+            Text("pi-label \(value)")
+            Button("\(buttonNumber) button-label", action: {
+                changeColor.toggle()
+                buttonNumber += 1
+            })
+            TextField(placeholder, text: $name)
+            Text(date, formatter: dateFormatter)
+        }.background(changeColor ? Color.red : Color.yellow)
+        
     }
 }
-
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .environment(\.locale, .init(identifier: "en"))
+            ContentView()
+                .environment(\.locale, .init(identifier: "es"))
+        }
+    }
 }
